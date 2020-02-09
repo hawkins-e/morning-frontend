a4259d315386ea14cf62fa25732ee8ca070f4b72
Erica Hawkins <ehawk901@gmail.com>
2020-02-09 00:01:44 -0600

    Margin Heading Re-Sizing

--- src/components/HomepageLayout.js
+++ src/components/HomepageLayout.js
@@ -84,7 +84,7 @@ class DesktopContainer extends Component {
           <Segment
             inverted
             textAlign='center'
-            style={{ minHeight: 700, padding: '1em 0em' }}
+            style={{ minHeight: 500, padding: '8em 0em' }}
             vertical
           >
 
